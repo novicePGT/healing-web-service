852ac39914055e870ca6cf014f3e161a1ef5e83b
novicePGT <dymnam@naver.com>
2023-03-24 21:06:38 +0900

    [refactor]: 메인페이지 변경

--- src/main/frontend/src/components/Main.jsx
+++ src/main/frontend/src/components/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ocean from '../assets/ocean.mp4'
+import mainvideo from '../assets/mainvideo.mp4'
 
 const Main = () => {
   return (
     <div className='main'>
         <div className="overlay"></div>
-        <video src={ocean} autoPlay loop muted />
+        <video src={mainvideo} autoPlay loop muted />
         <div className='content'>
             <h1>Welcome</h1>
             <p>To Healing WebPage.</p>
