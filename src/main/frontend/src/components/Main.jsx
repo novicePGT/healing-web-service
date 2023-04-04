d1efd28b450bc3179cef0f996dd926a32d67242c
novicePGT <dymnam@naver.com>
2023-04-05 01:00:00 +0900

    [chore]: 파일 날라감의 오류로 다시 재 초기화

--- src/main/frontend/src/components/Main.jsx
+++ src/main/frontend/src/components/Main.jsx
@@ -6,9 +6,13 @@ const Main = () => {
     <div className='main'>
         <div className="overlay"></div>
         <video src={mainvideo} autoPlay loop muted />
-        <div className='content'>
+        <div className='contentCenter'>
             <h1>Welcome</h1>
             <p>To Healing WebPage.</p>
+        </div>
+        <div className='contentTop'>
+          <h3>This place is gathering of people need to rest.</h3>
+          <h5>Do you want a break?</h5>
         </div>
     </div>
   )
