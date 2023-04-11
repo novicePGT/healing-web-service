d8bafb45d49623d35af487a69b0c1fe781a59ef8
novicePGT <dymnam@naver.com>
2023-04-11 23:41:36 +0900

    [feat-back]: imageItem을 저장하기 위한 imageItem 클래스 추가

--- src/main/java/kr/jj/healingwebservice/domain/item/ImageItem.java
+++ src/main/java/kr/jj/healingwebservice/domain/item/ImageItem.java
@@ -1,2 +1,32 @@
-package kr.jj.healingwebservice.domain.item;public class ImageItem {
+package kr.jj.healingwebservice.domain.item;
+
+import lombok.Data;
+
+/**
+ * 이 클래스는 이미지의 데이터를 담기 위해서 만들어진 클래스임.
+ * @Data -> 모든 필드에 대해 Getter와 Setter를 자동으로 생성 + equals(), hashCode(), toString()
+ */
+
+@Data
+public class ImageItem {
+
+    private int id;
+    private String imageName;
+    private String imageUrl;
+
+    /**
+     * 매개변수 없는 빈 생성자를 생성한 이유는 다음과 같다.
+     * Java Bean 규약을 따르기 위함.
+     * 객체 직렬화를 위해서는 매개변수 없는 생성자가 필요하다
+     * -> 직렬화 이유: 객체를 영구적으로 저장하고 복원 가능 + 캐시에 저장 가능 + 네트워크로 전송 가능 + 분산 환경에서 객체 공유 가능
+     */
+    public ImageItem() {
+
+    }
+
+    public ImageItem(int id, String imageName, String imageUrl) {
+        this.id = id;
+        this.imageName = imageName;
+        this.imageUrl = imageUrl;
+    }
 }
