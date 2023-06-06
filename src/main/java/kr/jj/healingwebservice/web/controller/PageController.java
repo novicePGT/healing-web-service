51e8d79a796c730d67d1c348189d57356e372e94
novicePGT <dymnam@naver.com>
2023-06-06 18:19:45 +0900

    [feat]: Controller

--- src/main/java/kr/jj/healingwebservice/web/controller/PageController.java
+++ src/main/java/kr/jj/healingwebservice/web/controller/PageController.java
@@ -1,2 +1,27 @@
-package kr.jj.healingwebservice.web.controller;public class PageController {
+package kr.jj.healingwebservice.web.controller;
+
+import kr.jj.healingwebservice.domain.item.ImageItem;
+import kr.jj.healingwebservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class PageController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping("/{country}")
+    public String images(@PathVariable String country, Model model) {
+        List<ImageItem> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return country;
+    }
 }
